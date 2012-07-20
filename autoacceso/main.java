aa042b09062b1e3d92feb2cb05ceafa6be55ff49
Victor Alfonso Esparza Reyes <va_esparza@hotmail.com>
2012-07-20 09:49:04 -0500

    Vista de entrada inicial

---
+++ autoacceso/main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autoacceso;
+
+/**
+ *
+ * @author VAERON
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Alumnos hola=new Alumnos();
+        hola.setVisible(true);
+    }
+}
